4552c0bd34b12da74781f6931b525993c1a4e01e
Hhellobro <154648634+Hhellobro@users.noreply.github.com>
2023-12-29 12:44:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,8 +34,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hellobro:Su1&7@9$2?11@cluster0.gtgqxwe.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "hellobro")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #DATABASE_URI = DATABASE_URI.replace("://", "://{}:{}@".format(quote_plus(username), quote_plus(password)))
 
